c509fee265afc905355337c74f2b16ec77f93a95
nealmenon <nealmenon@gmail.com>
2020-07-02 10:46:23 +0530

    Complete test for adding

---
+++ 3-types/SimpleMath.go
@@ -0,0 +1,6 @@
+package types
+
+// Add takes 2 ints and return sum
+func Add(a, b int) int {
+	return a + b
+}
---
+++ 3-types/SimpleMath_test.go
@@ -0,0 +1,22 @@
+package types
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func TestAdd(t *testing.T) {
+	rand.Seed(time.Now().UnixNano())
+	x := rand.Intn(100)
+	y := rand.Intn(100)
+	sum := Add(x, y)
+	expected := x + y
+
+	if sum != expected {
+		t.Errorf("expected '%d' but got '%d'", expected, sum)
+	} else {
+		fmt.Printf("x = %d and y = %d\n", x, y)
+	}
+}
